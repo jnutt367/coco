676d286844e6cd7f5fc03b8fdb0c516e3de11d67
Jason Nutt <jnutt367@gmail.com>
2021-04-11 15:34:03 -0400

    commit for small screens

--- gsap.js
+++ gsap.js
@@ -2,4 +2,4 @@ gsap.fromTo(".first", { x: 1, opacity: "0" }, { duration: 6, rotate: 360, border
 
 gsap.to(".second", { duration: 2, y: 200 });
 
-gsap.fromTo(".coco", { color: "blue" }, { duration: 8, y: 200, color: "purple", textShadow: "5px 5px pink", fontSize: "100px", fontWeight: "900", fontFamily: "fantasy" })
+gsap.fromTo(".coco", { color: "blue" }, { duration: 8, y: 200, color: "purple", textShadow: "5px 5px pink", fontSize: "60px", fontWeight: "900", fontFamily: "fantasy" })

1b8b8c882f22b8d0ff03fda1fd68361beaec3bb1
Jason Nutt <jnutt367@gmail.com>
2021-04-11 15:47:02 -0400

    commit for todays practice

--- gsap.js
+++ gsap.js
@@ -1,5 +1,5 @@
-gsap.fromTo(".first", { x: 1, opacity: "0" }, { duration: 6, rotate: 360, borderRadius: "50%", y: 100, opacity: "1" });
-
+gsap.fromTo(".first", { x: 1, opacity: "0" }, { duration: 6, rotate: 360, scale: 2, borderRadius: "50%", y: -10, border: "2px inset blue", opacity: "1" });
+gsap.fromTo(".stars", { x: 1, opacity: "0" }, { duration: 6, rotate: 360, scale: 2, borderRadius: "50%", y: -10, opacity: "1" });
 gsap.to(".second", { duration: 2, y: 200 });
 
 gsap.fromTo(".coco", { color: "blue" }, { duration: 8, y: 200, color: "purple", textShadow: "5px 5px pink", fontSize: "60px", fontWeight: "900", fontFamily: "fantasy" })
